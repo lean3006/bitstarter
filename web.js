12bb7da295e7f99203f89e0ab23dd8188d2475d1
Ubuntu <ubuntu@ip-172-31-0-152.us-west-2.compute.internal>
2013-07-13 21:01:43 +0000

    Web

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
-var express = require('express');
+1;3201;0cvar express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require ('fs');
 
-buf = fs.readFileSync('https://github.com/lean3006/bitstarter/blob/master/index.html');
+buf = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   len=buf.length;
