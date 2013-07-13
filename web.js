6850390c2c61b6f04e82dc3469c92758159ac676
Ubuntu <ubuntu@ip-172-31-0-152.us-west-2.compute.internal>
2013-07-13 20:54:10 +0000

    change Wb.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require ('fs');
 
-buf = fs.readFileSync('index.html');
+buf = fs.readFileSync('https://github.com/lean3006/bitstarter/blob/master/index.html');
 
 app.get('/', function(request, response) {
   len=buf.length;
